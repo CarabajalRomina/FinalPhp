a7d093bd7ed7d7a70826a3574617764d8dbe836f
RominaCarabajal <romicarabajal1998@gmail.com>
2023-12-10 20:22:26 -0300

    Subiendo proyecto php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once "./vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class='container'><h1>contenido principa</h1></div>
+<!--FIN del cont principal-->
+<?php require_once "./vistas/parte_inferior.php"?>
